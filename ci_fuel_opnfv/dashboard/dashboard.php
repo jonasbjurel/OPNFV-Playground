b0f20c186115b16018072040ac5e49e251896125
Jonas Bjurel <jonasbjurel@hotmail.com>
2015-09-03 21:09:51 +0200

    --DO NOT MERGE-- This is the first commit of a web-based dashboard for the CI system Things todo: - Rebase - Fix instrumentation of the control panel - Fix VM stats (CPU%, MEM%, IO%) - Fix Console auto-scrolling and the loop blocking the updating of the dashboard - General tidying up, indents, whitespaces, etc - UX Art styling and icons
    Signed-off-by: Jonas Bjurel <jonasbjurel@hotmail.com>

---
+++ ci_fuel_opnfv/dashboard/dashboard.php
@@ -0,0 +1,82 @@
+<!--#########################################################################-->
+<!--# Copyright (c) 2015 Jonas Bjurel and others.                            -->
+<!--# jonasbjurel@hotmail.com                                                -->
+<!--# All rights reserved. This program and the accompanying materials       -->
+<!--# are made available under the terms of the Apache License, Version 2.0  -->
+<!--# which accompanies this distribution, and is available at               -->
+<!--# http://www.apache.org/licenses/LICENSE-2.0                             -->
+<!--#########################################################################-->
+
+<html>
+  <head>
+    <title> OPNFV CI Dashboard </title>
+<link rel="stylesheet" type="text/css" href="dash-stylesheet.css">
+<!--    <meta http-equiv="refresh" content="60" /> -->
+  </head>
+
+  <body>
+    <?php
+      include 'php/get_metadata.php';
+    ?>
+
+    <div class="wrapper">
+      <!--  <header class="header">Header</header> -->
+     <article class="header">
+       <p>
+         <font size="6" color="green"><b>OPNFV@FUEL Developer's pipeline Dashboard</b></font></br>
+         <img style="vertical-align:middle" src="images/LF.jpeg" height="90" width="150">
+         <img style="vertical-align:middle" src="images/opnfv-extended.jpeg" height="75" width="150">
+         <img style="vertical-align:middle" src="images/openstack.jpeg" height="150" width="150">
+         <img style="vertical-align:middle" src="images/opendaylight.png" height="75" width="170">
+         <img style="vertical-align:middle" src="images/fuel.png" height="75" width="160">
+       </p>
+     </article>
+
+     <article class="ci-status">
+       <p>
+         CI-Pipeline status:
+         <?php
+           include('php/status.php');
+         ?>
+       </p>  
+     </article>
+
+     <article class="vm-status">
+       <p>
+         KVM VM-status:
+         <?php
+           include('php/vm-status.php');
+         ?>
+       </p>  
+     </article>
+
+     <article class="ci-history">
+       <p>
+         CI-History:
+         <?php
+           include('php/history.php');
+         ?>
+       </p>  
+     </article>
+
+     <article class="ci-control">
+       <p>
+         CI-Control panel:
+           <?php
+             include('php/control.php');
+           ?>
+         </p>  
+       </article>
+
+       <article class="ci-console">
+         <p>
+           CI-Console:
+           <?php
+             include('php/console.php');
+           ?>
+         </p>  
+       </article>
+
+     </div>
+  </body>
+</html>
---
+++ ci_fuel_opnfv/dashboard/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2015 Jonas Bjurel and others.
+# jonasbjurel@hotmail.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# NEEDS MORE WORK!, NOT TESTED AT ALL! 
+sudo apt-get install git python-pip python-libvirt python-libxml2 novnc supervisor nginx php5 php5-dev php-pear libyaml-dev
+
+sudo pecl install yaml-0.6.3
+
+sudo adduser www-data libvirtd
+
+echo "edit the /etc/apache2/conf.d/yaml.ini file by:"
+echo "$ cd /etc/apache2/conf.d"
+echo "$ sudo vi yaml.ini"
+echo "add the following line:  extension=yaml.so"
+
+echo "restar the Apache 2 server:"
+echo "$ sudo apache2ctl restart"
